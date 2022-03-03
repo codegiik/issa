87986f4c3aabbd34f2e19f441de645feae318a95
Giuseppe Pascale <giuseppe.pascale03@gmail.com>
2022-03-03 15:24:41 +0100

    :sparkles: Adding Hero and Heading components

---
+++ frontend/components/Heading.js
@@ -0,0 +1,15 @@
+import style from 'styles/components/heading.module.css'
+
+export function Heading({ children, type, className }){
+  const HeadingType = type
+
+  return (
+    <HeadingType className={[Array.isArray(className) ? className.join(' ') : className, style.heading].join(' ')}>
+      {children}
+    </HeadingType>
+  )
+}
+
+Heading.defaultProps = {
+  type: 'h2'
+}
