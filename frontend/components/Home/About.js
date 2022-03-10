d808a04139fe9d19bc8aad9e532fb3748cd20d58
Giuseppe Pascale <giuseppe.pascale03@gmail.com>
2022-03-11 00:50:32 +0100

    :sparkles: Adding About Section

--- frontend/components/Home/About.js
+++ frontend/components/Home/About.js
@@ -9,6 +9,10 @@ export function About ({}) {
         Chi siamo?
       </Heading>
       <section id="about" className={style.aboutSection}>
+        <div className={style.aboutTextContainer}>
+          <div className={style.aboutText}>ISSA è un'associazione di docenti, studenti e altre persone interessate al mondo della scuola e del territorio, con sede in Pozzuoli (NA). Ha per fini la promozione della cultura scientifica come benessere individuale, valore sociale e parte integrante del sapere.</div>
+          <div className={style.aboutText}>L'Associazione promuove ed organizza, senza alcuna finalità lucrativa, manifestazioni culturali, laboratoriali, e partecipa con propri soci a quelle promosse ed organizzate da altre Associazioni, Enti Pubblici e Privati; organizza convegni, dibattiti, conferenze, concorsi; svolge ricerca e sperimentazione didattica.</div>
+        </div>
       </section>
     </>
   )
