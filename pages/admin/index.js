f9693b51fe81e18a261d4b0b463dbdd2031ce138
Giuseppe Pascale <giuseppe.pascale03@gmail.com>
2022-05-17 15:18:50 +0200

    :lipstick: Styling Admin Sidebar and Home

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,7 +1,11 @@
 import { AdminLayout } from 'layouts/Admin';
 
 export default function AdminPanel({ session }) {
-    return <div>Things</div>;
+    return (
+        <div className="h-full w-full flex items-center justify-center">
+            <h3 className="font-serif font-bold text-4xl">🚧 In costruzione</h3>
+        </div>
+    );
 }
 
 AdminPanel.getLayout = (page) => <AdminLayout>{page}</AdminLayout>;
