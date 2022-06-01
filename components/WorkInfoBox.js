6939c4d76efd7c39756a7674e3777b6d8b4545a5
Giuseppe Pascale <giuseppe.pascale03@gmail.com>
2022-06-01 18:20:17 +0200

    :sparkles: Introducing Google Docs Embed

--- components/WorkInfoBox.js
+++ components/WorkInfoBox.js
@@ -12,6 +12,13 @@ export function Embed({ url, className, width, height }) {
             return `https://www.youtube.com/embed/${value[3]}?autoplay=0&origin=${window.location.origin}&controls=0&rel=1`;
         else if (
             (value =
+                /https?:\/\/(www\.)?docs\.google\.com\/(.+)(\/view|edit)/gi.exec(
+                    url
+                ))
+        ) {
+            return `https://docs.google.com/${value[2]}/preview`;
+        } else if (
+            (value =
                 /https?:\/\/(www\.)?drive\.google\.com\/file\/(.+)(\/view)/gi.exec(
                     url
                 ))
