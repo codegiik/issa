d731960114341ff934867bba3eeef115ebf576f9
vacwmX <antonio@codegiik.com>
2022-09-02 17:55:30 +0200

    added unwrap functions for strapi response objects

--- lib/strapi.ts
+++ lib/strapi.ts
@@ -1,4 +1,4 @@
-import Strapi from 'strapi-sdk-js';
+import Strapi, { StrapiResponse } from 'strapi-sdk-js';
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:1337';
 
@@ -6,7 +6,7 @@ const strapi = new Strapi({
     url: BASE_URL,
     prefix: '/api',
     store: {
-        key: '6660f4fdaf7e94d0dc69b2b2c6c63bbc1d7a0e4621f80bc788465f045430caea411a74c8e3c69ada468a9cfba9757ad549c93b1edbec95b6f22cf49c3a5bc819ca154d9c59b4c0fe7fdaf768662204037f512f208328fb47967686e687c58f3ef49933b1955439a2a3eb4284544a2069c2625ea0e19c7bf9091abb5a04916239',
+        key: process.env.NEXT_PUBLIC_STRAPI_TOKEN || '',
         useLocalStorage: true,
     },
 });
@@ -25,6 +25,22 @@ export const getFileUrl = (
     else url = record[param]?.data?.attributes?.url;
 
     return getCompleteUrl(url);
+};
+
+export const unwrap = (data: Record<string, any> | Record<string, any>[]) => {
+    if (!data) return;
+
+    if (!Array.isArray(data)) {
+        return {
+            ...data.attributes,
+            id: data.id,
+        };
+    }
+
+    return data.map((element) => ({
+        ...element.attributes,
+        id: element.id,
+    }));
 };
 
 export default strapi;
