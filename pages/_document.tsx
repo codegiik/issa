227b6078cf23809aa5fb76ea24279fa86491f691
Giuseppe Pascale <giuseppe.pascale03@gmail.com>
2022-09-24 11:06:08 +0200

    :bento: Adding proper favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -33,6 +33,38 @@ export default class CustomDocument extends Document {
                         href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,500;1,400;1,500&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap"
                         rel="stylesheet"
                     />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="180x180"
+                        href="/icons/apple-touch-icon.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="32x32"
+                        href="/icons/favicon-32x32.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="16x16"
+                        href="/icons/favicon-16x16.png"
+                    />
+                    <link rel="manifest" href="/site.webmanifest" />
+                    <link
+                        rel="mask-icon"
+                        href="/icons/safari-pinned-tab.svg"
+                        color="#47151b"
+                    />
+                    <link rel="shortcut icon" href="/favicon.ico" />
+                    <meta name="apple-mobile-web-app-title" content="ISSA" />
+                    <meta name="application-name" content="ISSA" />
+                    <meta name="msapplication-TileColor" content="#ebdece" />
+                    <meta
+                        name="msapplication-config"
+                        content="/browserconfig.xml"
+                    />
+                    <meta name="theme-color" content="#ebdece" />
                 </Head>
                 <body>
                     <Main />
